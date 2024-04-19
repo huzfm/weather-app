b85fd12bffa1a13cb313b2c144a9805cb3723d76
huzfm <huzaaifmushtaq@gmail.com>
2024-04-19 18:21:09 +0530

    Arrow function

--- app.js
+++ app.js
@@ -5,53 +5,68 @@ const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
+const humidity = document.getElementById("humidity")
 const descriptionElement = document.getElementById('description');
+const country = document.getElementById("country")
+const lat = document.getElementById("lat");
+const long = document.getElementById("long")
 
 
-function handleInput(e){
-    if (e.keyCode === 13){
-      
+const fetchWeather = async (location) => {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        locationElement.textContent = data.name;
+        temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+        humidity.textContent = `Humidity - ${data.main.humidity}`
+        descriptionElement.textContent = data.weather[0].description;
+        long.textContent = `Longitude - ${data.coord.lon}`
+        lat.textContent = `Latitude - ${data.coord.lat}`
+        country.textContent = data.sys.country;
+        console.log(data)
+
+
+    }
+    catch (error) {
+        console.error("Error in fetching data", error);
+        locationElement.textContent = "Please enter a valid city name";
+        temperatureElement.textContent = "or check spelling";
+        descriptionElement.textContent = "";
+        country.textContent = ''
+        lat.textContent = ''
+        long.textContent = ''
+        humidity.textContent = ''
+        // locationElement.style = 'color: red';
+        // temperatureElement.style = 'color: red';
+        // descriptionElement.style = 'color: red';
+    }
+    
+}
+
+
+
+function handleInput(e) {
+    if (e.keyCode === 13) {
+
         const location = locationInput.value;
         if (location) {
             fetchWeather(location);
-        } 
+        }
     }
 }
+
 searchButton.addEventListener('click', (e) => {
-   
+
     const location = locationInput.value;
     if (location) {
         fetchWeather(location);
     }
-  
+
 });
 
 
 
 
-function fetchWeather(location) {
-    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            locationElement.textContent = data.name;
-            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            locationElement.textContent = "Please enter the valid City name";
-            temperatureElement.textContent = "or check"
-            descriptionElement.textContent = "spelling"
-            // locationElement.style = 'color : red'
-            // temperatureElement.style = 'color : red'
-            // descriptionElement.style = 'color : red '
-            
-
-
-        });
-
-
-}
 
